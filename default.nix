c67a8ed7af0218316a308abbbfa804b896319956
Tony Morris <tmorris@tmorris.net>
2017-08-18 08:51:11 +1000

    nix

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  sources = {
+    papa = pkgs.fetchFromGitHub {
+      owner = "qfpl";
+      repo = "papa";
+      rev = "97ef00aa45c70213a4f0ce348a2208e3f482a7e3";
+      sha256 = "0qm0ay49wc0frxs6ipc10xyjj654b0wgk0b1hzm79qdlfp2yq0n5";
+    };
+  };
+
+  modifiedHaskellPackages = haskellPackages.override {
+    overrides = self: super: import sources.papa self // {
+      parsers = pkgs.haskell.lib.dontCheck super.parsers;        
+    };
+  };
+
+  aviation-units = modifiedHaskellPackages.callPackage ./aviation-units.nix {};
+
+in
+
+  aviation-units
+
